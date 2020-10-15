b19b206b4a4e9ba392fc8a2822adc034471c96cc
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 17:31:31 -0300

    Adicionar endpoint para localtime

--- src/main/java/com/rest/cofeeandelk/api/server/DefaultRestApi.java
+++ src/main/java/com/rest/cofeeandelk/api/server/DefaultRestApi.java
@@ -1,7 +1,10 @@
 package com.rest.cofeeandelk.api.server;
 
 
+import java.time.LocalTime;
+
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
-	@RequestMapping(value = "/welcome")
+	@GetMapping(value = "/welcome")
 	public String welcome() {
 		System.out.println(StringUtils.join("Just give me"," coffee"," and"," TV"));
 		return "Just give me Coffe & TV, oops, Elastic Stack (ElasticSearch, Logstash and Kibana)!";
+	}
+	
+	@GetMapping(value = "/time")
+	public String time() {
+		return LocalTime.now().toString();
 	}
 
 }
