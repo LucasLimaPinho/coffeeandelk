c6ecdeec8d7c30e991dc23fa485e49d366bc6749
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 16:26:08 -0300

    Adicionar primeiro @RequestMapping com HelloWorld com SpringBoot

---
+++ src/main/java/com/rest/cofeeandelk/api/server/DefaultRestApi.java
@@ -0,0 +1,15 @@
+package com.rest.cofeeandelk.api.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class DefaultRestApi {
+	
+	@RequestMapping(value = "/welcome")
+	public String welcome() {
+		return "Welcome to Coffe & TV, oops, Elastic Stack (ElasticSearch, Logstash and Kibana)!";
+	}
+
+}
