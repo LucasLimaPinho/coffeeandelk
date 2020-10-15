54ee3512f1e997a5df518eb87db3e1af675eebf3
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 20:54:57 -0300

    Adicionar aleatoriadade para construção dos novos fields no POJO Car.java - firstReleaseDate, price & available

--- src/main/java/com/rest/cofeeandelk/service/RandomCarService.java
+++ src/main/java/com/rest/cofeeandelk/service/RandomCarService.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.stereotype.Service;
 
 import com.rest.cofeeandelk.entity.Car;
+import com.rest.cofeeandelk.util.RandomDateUtil;
 
 // Implementação da interface CarService.
 
@@ -22,7 +23,15 @@ public class RandomCarService implements CarService {
 		var brand = BRANDS.get(ThreadLocalRandom.current().nextInt(0, BRANDS.size()));
 		var color = COLORS.get(ThreadLocalRandom.current().nextInt(0, COLORS.size()));
 		var type = TYPES.get(ThreadLocalRandom.current().nextInt(0, TYPES.size()));
-		return new Car(brand,color,type);
+		var available = ThreadLocalRandom.current().nextBoolean();
+		var price = ThreadLocalRandom.current().nextInt(5000, 120001);
+		var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+		var result = new Car(brand, color, type);
+		result.setAvailable(available);
+		result.setPrice(price);
+		result.setFirstReleaseDate(firstReleaseDate);
+				
+		return result;
 	}
 
 }
