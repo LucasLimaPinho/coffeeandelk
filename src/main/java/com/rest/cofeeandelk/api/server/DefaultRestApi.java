4f4a58c37e17acbba037ea4cc42bce1a356987b5
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 17:05:05 -0300

    Adicionar variáveis em build.gradle

--- src/main/java/com/rest/cofeeandelk/api/server/DefaultRestApi.java
+++ src/main/java/com/rest/cofeeandelk/api/server/DefaultRestApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
-	
+
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
 		return "Just give me Coffe & TV, oops, Elastic Stack (ElasticSearch, Logstash and Kibana)!";
