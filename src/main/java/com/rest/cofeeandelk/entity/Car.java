934aa414dd59c07c45f5470eb760fdf755d471be
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 23:48:04 -0300

    Adicionar anotação @Field para lidar com date fields quando for fazer o mapping para o Elasticsearch

--- src/main/java/com/rest/cofeeandelk/entity/Car.java
+++ src/main/java/com/rest/cofeeandelk/entity/Car.java
@@ -3,6 +3,10 @@ package com.rest.cofeeandelk.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -17,6 +21,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 // Para construir nossa JSON Response, iremos no basear no modelo do POJO Car.java
 // Para ser serializado/marshealizado - a classe deve ser pública
 
+// Iremos utilizar o POJO Car.java como data para o ElasticSearch. Para isto, essa classe deve ter a anotação 
+// @Document
+
+@Document(indexName = "coffee-and-elk")
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Car {
 
@@ -24,18 +32,27 @@ public class Car {
 	
 	@JsonInclude(value = Include.NON_EMPTY)
 	private List<String> additionalFeatures;
-	
+
 	private boolean available;
 
 	private String brand;
-
+	
 	private String color;
-
+	
 	@JsonUnwrapped
 	private Engine engine;
 
-	@JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Jakarta")
+	
+	// marking this field para o Elasticsearch entender a formatação de data. Extremamente importante utilizar
+	// a anotação @Field e utilizar o formato ISO-8601 - yyyy-MM-dd
+	// Esse é o cuidado que precisamos ter quando fazer o mapping de date fields para o ElasticSearch.
+	// 
+	@Field
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Jakarta")
 	private LocalDate firstReleaseDate;
+
+	@Id
+	private String id;
 
 	private int price;
 
@@ -49,7 +66,7 @@ public class Car {
 	public Car() {
 
 	}
-	
+
 	public Car(boolean available, String brand, String color, LocalDate firstReleaseDate, int price, String type) {
 		super();
 		this.available = available;
@@ -59,7 +76,7 @@ public class Car {
 		this.price = price;
 		this.type = type;
 	}
-	
+
 	public Car(List<String> additionalFeatures, boolean available, String brand, String color,
 			LocalDate firstReleaseDate, int price, String type, Engine engine, List<Tire> tires) {
 		super();
@@ -73,7 +90,7 @@ public class Car {
 		this.engine = engine;
 		this.tires = tires;
 	}
-
+	
 	public Car(List<String> additionalFeatures, String secretFeature, boolean available, String brand, String color,
 			Engine engine, LocalDate firstReleaseDate, int price, List<Tire> tires, String type) {
 		super();
@@ -88,7 +105,7 @@ public class Car {
 		this.tires = tires;
 		this.type = type;
 	}
-
+	
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
@@ -119,16 +136,20 @@ public class Car {
 		return firstReleaseDate;
 	}
 
+	public String getId() {
+		return id;
+	}
+
 	public int getPrice() {
 		return price;
 	}
 
-	// Para o unmarshalling ocorrer, obrigatoriamente devemos ter um 'construtor
-	// vazio' - no-argumento constructor.
-
 	public String getSecretFeature() {
 		return secretFeature;
 	}
+
+	// Para o unmarshalling ocorrer, obrigatoriamente devemos ter um 'construtor
+	// vazio' - no-argumento constructor.
 
 	public List<Tire> getTires() {
 		return tires;
@@ -150,14 +171,14 @@ public class Car {
 		this.available = available;
 	}
 
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
 	// Gerar construtor padrão de um POJO - ALT+SHIFT+S
 	// ALT+SHIFT+S também permite realizar o "Sort Members" para melhor readability
 	// do código
 	// ALT+SHIFT+F no Eclipse permite corrigir identação
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
 
 	public void setColor(String color) {
 		this.color = color;
@@ -169,6 +190,10 @@ public class Car {
 
 	public void setFirstReleaseDate(LocalDate firstReleaseDate) {
 		this.firstReleaseDate = firstReleaseDate;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public void setPrice(int price) {
