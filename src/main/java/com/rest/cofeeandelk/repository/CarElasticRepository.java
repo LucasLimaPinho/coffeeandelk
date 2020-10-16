8140675c4d400558d4934e2ffe5da89f51027dc7
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-16 02:02:34 -0300

    Hm

--- src/main/java/com/rest/cofeeandelk/repository/CarElasticRepository.java
+++ src/main/java/com/rest/cofeeandelk/repository/CarElasticRepository.java
@@ -1,6 +1,7 @@
 package com.rest.cofeeandelk.repository;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rest.cofeeandelk.entity.Car;
 
@@ -8,7 +9,7 @@ import com.rest.cofeeandelk.entity.Car;
 
 // A segunda é o field identificador de Car, então usaremos String.
 
-
+@Repository
 public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
 	
 	
--- src/main/java/com/rest/cofeeandelk/service/CarService.java
+++ src/main/java/com/rest/cofeeandelk/service/CarService.java
@@ -15,11 +15,11 @@ import com.rest.cofeeandelk.entity.Car;
 
 public interface CarService {
 	
-	List<String> BRANDS = List.of("Toyota", "Honda", "Ford");
+	List<String> BRANDS = List.of("Toyota", "Honda", "Ford","BMW","Mitsubishi");
 	
-	List<String> COLORS = List.of("Red","Black", "White");
+	List<String> COLORS = List.of("Red","Black", "White","Blue","Silver");
 	
-	List<String> TYPES = List.of("Sedan","SUV","MPV");
+	List<String> TYPES = List.of("Sedan","SUV","MPV","Hatchback","Conversible");
 	
 	List<String> ADDITIONAL_FEATURES = List.of("GPS","alarm","sunroof","media player", "Leather seats");
 	
