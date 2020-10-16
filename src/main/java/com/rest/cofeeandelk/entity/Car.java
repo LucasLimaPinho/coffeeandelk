3eab365714a6b49252ed09f76caeb1cf6ff92c8a
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-15 22:56:46 -0300

    Mudar application.yml de uma forma que todos os fields na resposta da API estejam no formato snake_case

--- src/main/java/com/rest/cofeeandelk/entity/Car.java
+++ src/main/java/com/rest/cofeeandelk/entity/Car.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -30,6 +31,7 @@ public class Car {
 
 	private String color;
 
+	@JsonUnwrapped
 	private Engine engine;
 
 	@JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Jakarta")
