4d5580ffe358ef26a504453ecfa6ce4b686b4c09
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-16 00:02:48 -0300

    Criar as condições para CRUD em Elasticsearch com aplicação java usando @Field, @Repository

---
+++ src/main/java/com/rest/cofeeandelk/repository/CarElasticRepository.java
@@ -0,0 +1,16 @@
+package com.rest.cofeeandelk.repository;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.rest.cofeeandelk.entity.Car;
+
+// A primeira classe de ElasticsearchRepository deve ser a classe a ser armazenada em data storage do Elasticsearch!!!
+
+// A segunda é o field identificador de Car, então usaremos String.
+
+
+public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
+	
+	
+
+}
