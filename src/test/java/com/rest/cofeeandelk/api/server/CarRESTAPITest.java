6267ba008012ab2c5e94e908fe1d1443831c731f
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-18 17:28:42 -0300

    Add UT: HTTP POST into Elasticsearch using @Qualifier annotation and assertTimeout to check with the operations will last less then one second. Import for query_apis

--- src/test/java/com/rest/cofeeandelk/api/server/CarRESTAPITest.java
+++ src/test/java/com/rest/cofeeandelk/api/server/CarRESTAPITest.java
@@ -1,11 +1,17 @@
 package com.rest.cofeeandelk.api.server;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.rest.cofeeandelk.entity.Car;
@@ -19,33 +25,46 @@ class CarRESTAPITest {
 
 	@Test
 	void testRandom() {
-		
-		webTestClient.get().uri("/api/car/v1/random").exchange().expectBody(Car.class)
-		.value(car -> {
-			assertTrue(CarService.BRANDS.contains(car.getBrand()));
-			assertTrue(CarService.COLORS.contains(car.getColor()));
-		});
-		
+
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Testing loop :" + i);
+			webTestClient.get().uri("/api/car/v1/random").exchange().expectBody(Car.class).value(car -> {
+				assertTrue(CarService.BRANDS.contains(car.getBrand()));
+				assertTrue(CarService.COLORS.contains(car.getColor()));
+			});
+
+		}
+
 	}
 
-	@Test
-	void testEcho() {
-		fail("Not yet implemented");
-	}
+	// We inject a CarService bean into the carService field.
+	// The @Qualifier("randomCarService") specifies that it is a RandomCarService
+	// bean.
+	// CarService is an interface; RandomCarService implements CarService;
+	// So we can have multiple beans implementing the interface CarService;
+	// The annotation @Qualifier will uniquely specify what type of bean to be
+	// inject with loose-coupling
 
-	@Test
-	void testRandomCars() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testCount() {
-		fail("Not yet implemented");
-	}
+	@Autowired
+	@Qualifier("randomCarService")
+	private CarService carService;
 
 	@Test
 	void testSaveCar() {
-		fail("Not yet implemented");
+
+		var randomCar = carService.generateCar();
+
+		// Assert that the HTTP POST Request will last less than 1 second
+
+		for (int i = 0; i < 100; i++) {
+
+			System.out.println("Inserting " + i + " document in Elasticsearch");
+			assertTimeout(Duration.ofSeconds(1),
+					() -> webTestClient.post().uri("api/car/v1").contentType(MediaType.APPLICATION_JSON)
+							.bodyValue(randomCar).exchange().expectStatus().is2xxSuccessful());
+
+		}
+
 	}
 
 	@Test
